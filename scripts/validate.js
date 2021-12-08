ebbe98b02ec4f9b9136017fabbac41749b927556
andreaclae <andreaclae@gmail.com>
2021-12-08 14:09:19 -0500

    after tutor help

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,73 +1,74 @@
-var isValid = true;
-
 enableValidation({
-    formSelector: '.modal__form',
-    inputSelector: '.modal__input',
-    errorTextSelector: '.modal__error-text', 
-    submitButtonSelector: ".modal__submit-button",
-    inactiveButtonClass: "modal__button_disabled",
-})
-
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  errorTextSelector: ".modal__error-text",
+  submitButtonSelector: ".modal__submit-button",
+});
 
 function enableValidation(settings) {
-    const forms = document.querySelectorAll(settings.formSelector)
- forms.forEach((form => {
-     setEventListeners(form, settings);
- }))
-} 
+  const forms = document.querySelectorAll(settings.formSelector);
+  forms.forEach((form) => {
+    setEventListeners(form, settings);
+  });
+}
 
-function setEventListeners(form, settings){
- const inputs = form.querySelectorAll(settings.inputSelector);
- 
-inputs.forEach((input) => {
+function setEventListeners(form, settings) {
+  const inputs = form.querySelectorAll(settings.inputSelector);
+  const button = form.querySelector(settings.submitButtonSelector);
+
+  inputs.forEach((input) => {
     //add the event listenser
-    input.addEventListener('input', (event) => {
-        checkInputValidity(input, settings)
-    //check if it's valid
-    //check if all the inputs are valid
+    input.addEventListener("input", (event) => {
+      checkInputValidity(input, settings);
+      //check if it's valid
+      checkAllInputs(inputs, button);
+      //check if all the inputs are valid
+    });
+  });
+}
+function checkAllInputs(inputs, button) {
+  var inputArr = Array.from(inputs);
+  if (
+    inputArr.every((input) => {
+      return input.validity.valid;
     })
-    
-})
-
+  ) {
+    activateButton(button);
+  } else {
+    deactivateButton(button);
+  }
 }
 
 function checkInputValidity(input, settings) {
-   if(input.validity.valid){
-       removeErrorStyles(input)
-       isValid = true;
-   }else{
-       addErrorStyles(input)
-       isValid = false;
-   } 
-   return isValid
-   
+  if (input.validity.valid) {
+    removeErrorStyles(input);
+  } else {
+    addErrorStyles(input);
+  }
 }
 
-function removeErrorStyles(input, settings){
-    input.classList.remove('modal__input_has-error')
- input.nextElementSibling.classList.remove('modal__error-text_visible')
-
- const currentModalButton = input.form.querySelector(".modal__submit-button")
-     currentModalButton.disabled = false;
-     currentModalButton.classList.remove('modal__submit-button_disabled')
-
+function removeErrorStyles(input, settings) {
+  input.classList.remove("modal__input_has-error");
+  const span = document.querySelector(`#${input.id}-error`);
+  span.classList.remove("modal__error-text_visible");
 }
 
-
-function addErrorStyles(input, settings){
-    input.classList.add('modal__input_has-error')
-   input.nextElementSibling.classList.add('modal__error-text_visible')
-
-  const currentModalButton = input.form.querySelector(".modal__submit-button")
-    currentModalButton.disabled = true;
-    currentModalButton.classList.add('modal__submit-button_disabled')
+function addErrorStyles(input, settings) {
+  input.classList.add("modal__input_has-error");
+  const span = document.querySelector(`#${input.id}-error`);
+  span.classList.add("modal__error-text_visible");
 }
-
-
-
+function deactivateButton(button) {
+  button.disabled = true;
+}
+function activateButton(button) {
+  button.disabled = false;
+}
 //for each form
-    //get all inputs
-        //add event listener to each input
-        //check validity of that input
-        //check validity of that all inputs
-        //chnage the disabled status of submit button
+//get all inputs
+//add event listener to each input
+//check validity of that input
+//check validity of that all inputs
+//chnage the disabled status of submit button
+
+//validity should have error text as well use .textcontent to change text
